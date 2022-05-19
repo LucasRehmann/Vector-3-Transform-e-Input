4b9f1727ac344c8c9b16eada1df709aaa4bb51de
LucasRehmann <101809208+LucasRehmann@users.noreply.github.com>
2022-05-19 10:42:23 -0300

    Initial commit

---
+++ Assets/ObjectController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(movementSpeed,0,0);
+        }
+    }
+}
